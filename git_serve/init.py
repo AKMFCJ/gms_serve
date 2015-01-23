c19a667b926cf2970edfadaab2e532b81d3f1817
changjie.fan <changjie.fan@tinno.com>
2015-01-23 09:12:27 +0800

    git-serve-init

--- git_serve/init.py
+++ git_serve/init.py
@@ -1,10 +1,15 @@
 #-*- encoding:utf-8 -*-
 __author__ = 'changjie.fan'
-
+"""
+ssh authorized_keys的初始化
+.git-serve配置目录的初始化
+所有仓库中.git/hooks/update的初始化
+"""
 import os
 import logging
-
 import util
+import ConfigParser
+
 
 logger = logging.getLogger('git-serve')
 
@@ -38,6 +43,28 @@ class Main(object):
         if not os.path.exists(git_serve_dir):
             util.mk_dir(git_serve_dir)
             util.mk_dir(os.path.join(git_serve_dir, 'logs'))
+            Main.create_git_serve_conf(git_serve_dir)
+        else:
+            git_serve_conf = os.path.join(git_serve_dir, 'git-serve.conf')
+            if os.path.exists(git_serve_conf):
+                cfg = ConfigParser.RawConfigParser()
+                try:
+                    conf_file = file(git_serve_conf)
+                except (IOError, OSError), e:
+                    pass
+                try:
+                    cfg.readfp(conf_file)
+                finally:
+                    conf_file.close()
+                localhost_ip = util.get_localhost_ip('eth0')
+                if cfg.get('localhost', 'ip') != localhost_ip:
+                    cfg.set('localhost', 'ip', localhost_ip)
+                    cfg.write(open(git_serve_conf, 'w'))
+            else:
+                Main.create_git_serve_conf(git_serve_dir)
+
+    @staticmethod
+    def create_git_serve_conf(git_serve_dir):
             rfp = open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
                                     'git_serve/git-serve.conf'), 'r')
             wfp = open(os.path.join(git_serve_dir, 'git-serve.conf'), 'w')
